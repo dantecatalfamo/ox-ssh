218fed4558efb55c4e44209e8d5ec6d3f57c689c
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-10-11 16:50:45 -0400

    Begin translation from custom elisp to ox backend

--- ox-ssh.el
+++ ox-ssh.el
@@ -11,13 +11,30 @@
 (require 'ox)
 
 (org-export-define-backend 'ssh
-  '(()))
+                           '((headline . org-ssh-headline)
+                             (template . org-ssh-template)))
 
-(defun org-ssh-headline (headline contents info)
+(defun org-ssh-headline (headline _contents _info)
+  "Translate HEADLINE into SSH config line."
+  (let ((url (org-element-property :URL headline))
+        (ip (org-element-property :IP headline))
+        (host (car (org-element-property :title headline)))
+        (ssh-forward (org-element-property :SSH_FORWARD headline))
+        (ssh-port (org-element-property :SSH_PORT headline))
+        (user (org-element-property :SSH_USER headline)))
+    (when-let (addr (or ip url))
+      (concat "Host " host "\n"
+              "  HostName " addr "\n"
+              (when ssh-forward
+                "  ForwardAgent yes\n")
+              (when ssh-port
+                (concat "  Port " ssh-port "\n"))
+              (when user
+                (concat "  User " user "\n"))
+              "\n"))))
+
+(defun org-ssh-template (contents info)
   )
 
-(defin org-ssh-template (contents info)
-  )
-
-(provide 'os-ssh)
+(provide 'ox-ssh)
 ;;; ox-ssh.el ends here
