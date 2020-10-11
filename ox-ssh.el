d6420edf26964420b37bad670298b8ef74561324
Dante Catalfamo <dante.catalfamo@shopify.com>
2020-10-11 16:23:42 -0400

    Begin work on ox-ssh

---
+++ ox-ssh.el
@@ -0,0 +1,23 @@
+;;; ox-ssh --- SSH Config Backend for Org Export Engine -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Dante Catalfamo
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
+
+(require 'ox)
+
+(org-export-define-backend 'ssh
+  '(()))
+
+(defun org-ssh-headline (headline contents info)
+  )
+
+(defin org-ssh-template (contents info)
+  )
+
+(provide 'os-ssh)
+;;; ox-ssh.el ends here
