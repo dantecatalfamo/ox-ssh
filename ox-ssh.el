1763c6ff611f2103f9dee64a306e5f4d168d3785
Dante Catalfamo <dante.catalfamo@shopify.com>
2020-10-13 15:24:02 -0400

    Add ciphers, canonical domains, rearrange some

--- ox-ssh.el
+++ ox-ssh.el
@@ -34,13 +34,15 @@
          (ssh-user (org-element-property :SSH_USER headline))
          (ssh-port (org-element-property :SSH_PORT headline))
          (ssh-identity-file (org-element-property :SSH_IDENTITY_FILE headline))
-         (ssh-certificate-file (org-element-property :SSH_CERTIFICATE_FILE headline))
          (ssh-forward-agent (org-element-property :SSH_FORWARD_AGENT headline))
          (ssh-forward-x11 (org-element-property :SSH_FORWARD_X11 headline))
-         (ssh-address-family (org-element-property :SSH_ADDRESS_FAMILY headline))
          (ssh-add-keys-to-agent (org-element-property :SSH_ADD_KEYS_TO_AGENT headline))
+         (ssh-address-family (org-element-property :SSH_ADDRESS_FAMILY headline))
          (ssh-batch-mode (org-element-property :SSH_BATCH_MODE headline))
          (ssh-bind-interface (org-element-property :SSH_BIND_INTERFACE headline))
+         (ssh-canonical-domains (org-element-property :SSH_CANONICAL_DOMAINS headline))
+         (ssh-certificate-file (org-element-property :SSH_CERTIFICATE_FILE headline))
+         (ssh-ciphers (org-element-property :SSH_CIPHERS headline))
          (addr (or ip url)))
     (if addr
         (concat "\nHost " host "\n"
@@ -51,20 +53,24 @@
                   (concat "  Port " ssh-port "\n"))
                 (when ssh-identity-file
                   (concat "  IdentityFile " ssh-identity-file "\n"))
-                (when ssh-certificate-file
-                  (concat "  CertificateFile " ssh-certificate-file "\n"))
                 (when ssh-forward-agent
-                  "  ForwardAgent yes\n")
+                  (concat "  ForwardAgent " ssh-forward-agent "\n"))
                 (when ssh-forward-x11
-                  "  ForwardX11 yes\n")
+                  (concat "  ForwardX11 " ssh-forward-x11 "\n"))
+                (when ssh-add-keys-to-agent
+                  (concat "  AddKeysToAgent " ssh-add-keys-to-agent "\n"))
                 (when ssh-address-family
                   (concat "  AddressFamily " ssh-address-family "\n"))
-                (when ssh-add-keys-to-agent
-                  "  AddKeysToAgent yes\n")
                 (when ssh-batch-mode
-                  "  BatchMode yes\n")
+                  (concat "  BatchMode " ssh-batch-mode "\n"))
                 (when ssh-bind-interface
                   (concat "  BindInterface " ssh-bind-interface "\n"))
+                (when ssh-canonical-domains
+                  (concat "  CanonicalDomains " ssh-canonical-domains "\n"))
+                (when ssh-certificate-file
+                  (concat "  CertificateFile " ssh-certificate-file "\n"))
+                (when ssh-ciphers
+                  (concat "  Ciphers " ssh-ciphers "\n"))
                 contents)
       contents)))
 
