76020dd8ba04686d041fbbbc382508cdff70b4ab
Dante Catalfamo <dante.catalfamo@gmail.com>
2020-10-13 19:34:47 -0400

    Add package header

--- ox-ssh.el
+++ ox-ssh.el
@@ -1,6 +1,27 @@
-;;; ox-ssh --- SSH Config Backend for Org Export Engine -*- lexical-binding: t; -*-
+;;; ox-ssh.el --- SSH Config Backend for Org Export Engine -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2020 Dante Catalfamo
+
+;; Author: Dante Catalfamo
+;; Version: 1.0
+;; Package-Requires: ((emacs "24.4") (org "8.0"))
+;; Keywords: outlines, org, ssh
+;; Homepage: https://github.com/dantecatalfamo/ox-ssh
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;;; Commentary:
 
